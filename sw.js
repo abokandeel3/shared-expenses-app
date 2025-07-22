4b26d60e9334a26f68117d4fca21c81eed2a9d03
abokandeel3 <abokandeel3@gmail.com>
2025-07-22 14:05:50 +0100

    upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // اسم الكاش الذي سنخزن فيه ملفات التطبيق
-const CACHE_NAME = 'shared-expenses-cache-v3';
+const CACHE_NAME = 'shared-expenses-cache-v4';
 
 // قائمة الملفات الأساسية التي نريد تخزينها باستخدام المسارات النسبية الصحيحة
 const assetsToCache = [
